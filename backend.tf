30362d7fa874bdc4d69b53f776019b69b13fed0b
Nnamdi <podcohort4dev@gmail.com>
2025-11-26 22:02:18 +0100

    new tf state

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "digitalwitchngbucketcloud1"
-    key    = "digitalwitchng/prodution/terraform.tfstate"
-    region = "us-west-1"
+    bucket = "pod3-terraform-state-pod3-terraform-state "  # Your new bucket name
+    key    = "terraform.tfstate"              # Simplified path
+    region = "us-east-1"
   }
 }
